fde7d785693c712f4b53b49b04c27ce86b044056
Kilari Teja <kilariteja9@gmail.com>
2017-10-23 23:32:05 +0530

    added delayed invoking, pid dumping

--- playground/loader.py
+++ playground/loader.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
 import sys
-import time
+import time, os
 import argparse, json
 from glo import GLO
 from PySide.QtCore import *
 from PySide.QtGui import *
-from subprocess import Popen, PIPE
+from subprocess import call, Popen, PIPE
 from PySide.QtDeclarative import QDeclarativeView
 
 defaultKeys = GLO.defaultKeyStoreUrl(".glo/keys.json")
@@ -15,12 +15,21 @@ defaultPrefs = GLO.defaultKeyStoreUrl(".glo/prefs.json")
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-k", "--keys", type=str, default=defaultKeys, help="Twitter API Keys")
 argParser.add_argument("-p", "--prefs", type=str, default=defaultPrefs, help="Preference Keys")
+argparse.add_argument("--delayed", action="store_true", help="Initially Delay")
 
 parsed = argParser.parse_args()
 
 # load the Glo Instance
 glo = GLO(parsed.keys, parsed.prefs)
 every = int(glo.getPref("every"))
+
+# delay if needed
+if parsed.delayed:
+	time.time(every*4)
+
+# dump away the pid
+with open(os.path.join(os.path.expanduser("~"), ".glo/.pid"), "w") as to:
+	to.write(os.getpid())
 
 while True:
 
@@ -47,13 +56,25 @@ while True:
 	result.wait()
 
 	# result as json
-	resultJson = result.stdout.read().strip()
-	resultJson = json.loads(resultJson)
+	try:
+		resultJson = result.stdout.read().strip()
+		resultJson = json.loads(resultJson)
+	except:
+		resultJson = { "ok": False }
 
 	if resultJson["ok"]:
 		try:
 			result = glo.postTweet(resultJson["tweet"])
-			print "[!] Send Tweet Status: {}".format(result)
-		except: pass
+
+			# use linux native notify-send
+			call([
+				"notify-send", "--urgency=critical",
+				"Tweeted Successfully!",
+				str(resultJson["tweet"])
+			])
+		except:
+			call([
+				"notify-send", "--urgency=critical", "Tweeting Failed!"
+			])
 	else:
-		print "[!] Tweet Cancelled!"
+		pass
