2aaf7477d901fb21a5ff3c8143292e23430635bc
Kilari Teja <kilariteja9@gmail.com>
2017-10-23 23:32:52 +0530

    added ability to terminate GLO

---
+++ playground/glo-conf.py
@@ -0,0 +1,42 @@
+"""
+	@author ksdme
+"""
+import os
+from subprocess import call
+from argparse import ArgumentParser
+
+argParser = ArgumentParser()
+
+# stop/restart current thread
+subparsers = argParser.add_subparsers(dest="mode")
+
+# Stop GLO Subparser
+stopSub = subparsers.add_parser("stop", help='Stop GLO')
+
+# the deed has been done
+parsed = argParser.parse_args()
+
+#----------- Utility Funcs -----------
+def getPID(loc=None):
+	if loc is None:
+		loc = os.path.join(os.path.expanduser("~"), ".glo/.pid")
+
+	try:
+		with open(loc, "r") as locR:
+			return int(locR.read())
+	except:
+		return None
+
+# begin control flow stuff here
+if parsed.mode == "stop":
+	"""
+		remembr the current method wont
+		work if glo was started with root previls
+	"""
+
+	pid = getPID()
+	if pid is not None:
+		call(["kill", "-9", str(pid)])
+		print "Killed process with ", pid
+	else:
+		print "~/.glo/.pid missing"
